d9e1796420c097ad81862b286753462ef44e6429
clsaad <clsaad@student.42lyon.fr>
2021-06-15 11:51:34 +0200

    add NOT NORMED AT ALL instruction printing

--- src/main.c
+++ src/main.c
@@ -5,6 +5,33 @@
 
 #include <unistd.h>
 
+static void	print_op(enum e_ops op)
+{
+	if (op == SWAP_A)
+		write(STDOUT_FILENO, "sa\n", 3);
+	else if (op == SWAP_B)
+		write(STDOUT_FILENO, "sb\n", 3);
+	else if (op == SWAP_AB)
+		write(STDOUT_FILENO, "ss\n", 3);
+	else if (op == PUSH_A)
+		write(STDOUT_FILENO, "pa\n", 3);
+	else if (op == PUSH_B)
+		write(STDOUT_FILENO, "pb\n", 3);
+	else if (op == ROT_A)
+		write(STDOUT_FILENO, "ra\n", 3);
+	else if (op == ROT_B)
+		write(STDOUT_FILENO, "rb\n", 3);
+	else if (op == ROT_AB)
+		write(STDOUT_FILENO, "rr\n", 3);
+	else if (op == REV_ROT_A)
+		write(STDOUT_FILENO, "rra\n", 3);
+	else if (op == REV_ROT_B)
+		write(STDOUT_FILENO, "rrb\n", 3);
+	else if (op == REV_ROT_AB)
+		write(STDOUT_FILENO, "rrr\n", 3);
+	
+}
+
 #include <stdio.h>
 int	main(int argc, char *argv[])
 {
@@ -16,21 +43,15 @@ int	main(int argc, char *argv[])
 	if (ret != RTINIT_OK)
 	{
 		write(STDERR_FILENO, "Error\n", 6);
-		return (2);
+		return (1);
 	}
-	for (int i = argc - 2; i >= 0; --i) {
-		printf("[%d]: %d\n", argc - 2 - i, rt_ptr()->stack_a.data[i]);
-	}
+	// for (int i = argc - 2; i >= 0; --i) {
+	// 	printf("[%d]: %d\n", argc - 2 - i, rt_ptr()->stack_a.data[i]);
+	// }
 	// sort();
-	quick_sort(rt_ptr());
-	puts("STACK A");
-	for (int i = rt_ptr()->stack_a.elem_count - 1; i >= 0; --i) {
-		printf("[%zu]: %d\n", rt_ptr()->stack_a.elem_count - 1 - i, rt_ptr()->stack_a.data[i]);
-	}
-	puts("STACK B");
-	for (int i = rt_ptr()->stack_b.elem_count - 1; i >= 0; --i) {
-		printf("[%zu]: %d\n", rt_ptr()->stack_b.elem_count - 1 - i, rt_ptr()->stack_b.data[i]);
-	}
-	// rt_exit(!quick_sort(rt_ptr()));
+	if (!quick_sort(rt_ptr()))
+		rt_exit(2);
+	for (size_t i = 0; i < rt_ptr()->ops.elemcount; ++i)
+		print_op(((enum e_ops *)(rt_ptr()->ops.data))[i]);
 	rt_exit(0);
 }
